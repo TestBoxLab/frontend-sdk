03ae4421ea68057a8b7a0cd4aee0d07de08f805b
Jake Wood <jake@jakewood.org>
2022-11-10 17:46:50 -0600

    tests: add cypress tests

---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,18 @@
+describe('testbox script', () => {
+  it('sends message to TestBox', () => {
+    cy.visit('http://localhost:8080', {
+      onBeforeLoad: (win) => {
+        cy.spy(win, 'postMessage').as('postMessage');
+      }
+    });
+
+    cy.get('@postMessage').should('have.been.calledOnceWith', {
+      testbox: {
+        version: 1,
+        sender: "partner",
+        event: "initialize-request",
+        data: undefined,
+      }
+    }, "https://app.testbox.com");
+  })
+});
