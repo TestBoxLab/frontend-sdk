9840661f122a0c43d4674ece8c0601a0fab6d9b9
Jake Wood <jake@jakewood.org>
2022-11-16 11:20:32 -0600

    fix: do not listen to messages before start

--- src/index.ts
+++ src/index.ts
@@ -7,26 +7,27 @@ import {
 import { routeMessage } from "./router";
 import { INITIALIZE_REQUEST_EVENT } from "./messaging/outgoing";
 
-window.addEventListener("message", (ev) => {
-  if (ev.origin !== getTargetOrigin()) {
-    info("target-mismatch", {
-      messageOrigin: ev.origin,
-      targetOrigin: getTargetOrigin(),
-    });
-    return;
-  }
-
-  const { data } = ev;
-
-  if (!isValidIncomingTestBoxMessage(data)) {
-    info("not-a-testbox-message", { data });
-    return;
-  }
-
-  routeMessage(data);
-});
-
 export function startTestBox(config?: TestBoxConfig) {
   window.__tbxConfig = config;
+
+  window.addEventListener("message", (ev) => {
+    if (ev.origin !== getTargetOrigin()) {
+      info("target-mismatch", {
+        messageOrigin: ev.origin,
+        targetOrigin: getTargetOrigin(),
+      });
+      return;
+    }
+
+    const { data } = ev;
+
+    if (!isValidIncomingTestBoxMessage(data)) {
+      info("not-a-testbox-message", { data });
+      return;
+    }
+
+    routeMessage(data);
+  });
+
   sendMessageToTestBox(INITIALIZE_REQUEST_EVENT);
 }
